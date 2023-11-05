97b4626069a6a883f71821e19a4e27a874bd5c34
Dmytro Vozniuk <dmytro.vozniuk@knu.com>
2023-11-05 14:07:07 +0200

    delete

---
+++ lab4.var3.py
@@ -0,0 +1,14 @@
+# lab 4
+# var 3
+# Створення класів бібліотеки та книги.
+# quantity = "кількість сторінок книги"
+# number = "номер сторінки, до якої книгу було прочитано"
+# percent = "відсокток книги, який було прочитано"
+
+class Book():
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+    def percent():
+        
+        
